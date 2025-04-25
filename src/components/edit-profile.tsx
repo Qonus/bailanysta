839dd174fece5db3950c039bbf5d2d39e72dc669
Qonus <94901082+Qonus@users.noreply.github.com>
2025-04-26 01:54:03 +0500

    [Fix] build errors

--- src/components/edit-profile.tsx
+++ src/components/edit-profile.tsx
@@ -19,7 +19,7 @@ export default function EditProfileButton() {
                 <DialogHeader>
                     <DialogTitle>{t("edit")}</DialogTitle>
                     <DialogDescription>
-                        Make changes to your profile here. Click save when you&apos;re done.
+                        {t("edit-description")}
                     </DialogDescription>
                 </DialogHeader>
                 <div className="grid gap-4 py-4">
