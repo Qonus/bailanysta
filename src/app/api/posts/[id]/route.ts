0fbfa48016864a5bd1c67456e9f4e352a29c3f84
Qonus <94901082+Qonus@users.noreply.github.com>
2025-04-26 21:43:49 +0500

    Update route.ts

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -1,8 +1,10 @@
 import { db } from "@/lib/db";
 import { posts } from "@/lib/db/schema";
 import { eq } from "drizzle-orm";
+import { NextRequest } from "next/server";
 
 export async function GET(
+    _request: NextRequest,
     { params }: { params: Promise<{ id: string }> },
 ) {
     const id = (await params).id;
