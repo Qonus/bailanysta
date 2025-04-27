de297b88670a586fcbec74d05e88926906851719
Qonus <94901082+Qonus@users.noreply.github.com>
2025-04-27 13:25:24 +0500

    Update top-bar.tsx

--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -2,7 +2,7 @@ import BackButton from "./back-button";
 
 export default function TopBar({ title }: { title: string }) {
     return (
-        <div className="h-13 border-b-1 border-b-current/20 flex items-center px-6 justify-start gap-5 backdrop-blur-lg sticky top-0">
+        <div className="z-50 h-13 border-b-1 border-b-current/20 flex items-center px-6 justify-start gap-5 backdrop-blur-lg sticky top-0">
             <BackButton />
             <p className="text-lg font-bold">
                 {title}
