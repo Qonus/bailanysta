959fa15c331a01aad22cac7a96d239ea5441e0fa
Qonus <94901082+Qonus@users.noreply.github.com>
2025-04-27 10:56:19 +0500

    Update route.ts

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,16 +1,25 @@
 import { db } from "@/lib/db";
 import { posts } from "@/lib/db/schema";
-import { desc, eq } from "drizzle-orm";
 import { NextRequest } from "next/server";
 
-export async function GET(request: NextRequest) {
-    const searchParams = request.nextUrl.searchParams;
-    const userId = searchParams.get('userid');
+export async function GET(
+    request: NextRequest   // eslint-disable-line @typescript-eslint/no-unused-vars
+) {
+    const data = [
+        {
+            id: '0eeb3d20-5dc0-4666-9249-eb88dbb712cd',
+            userId: '7804cd82-0e7f-4c8c-96d3-0332f4695175',
+            content: 'I just deleted my whole database, a I cooked?',
+            created_at: '2025-04-27T03:14:11.487Z'
+        }
+    ];
+    // const searchParams = request.nextUrl.searchParams;
+    // const userId = searchParams.get('userid');
 
-    const data = await db.query.posts.findMany({
-        orderBy: [desc(posts.created_at)],
-        where: userId ? eq(posts.userId, userId as string) : undefined,
-    });
+    // const data = await db.query.posts.findMany({
+    //     orderBy: [desc(posts.created_at)],
+    //     where: userId ? eq(posts.userId, userId as string) : undefined,
+    // });
     return new Response(JSON.stringify(data), {
         status: 200,
         headers: { 'Content-Type': 'application/json' }
